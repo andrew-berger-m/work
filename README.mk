92708e004dff3c0f8ffbf91238ca7754b135ae65
Andrew Berger <andrew-berger-m@users.noreply.github.com>
2018-11-13 04:29:29 +0100

    Initial commit
    Added README.mk and .gitignore

---
+++ README.mk
@@ -0,0 +1,23 @@
+# Personal research project of Andrew Berger
+
+
+## Nice and honesty policy of the repository.
+
+1. Purpose of the repository is only my personal research in C++.
+
+1. You only can enter to the repository or access data if you wanna do something nice for me or for the project.
+
+1. Otherwise, all data and metadata does not have license. You do not have right to misuse, download, modify, analyze, inspect, aggregate data or try to identify my person based on data.
+
+1. If you love contributing in the GitHub projects, you have following two options:
+
+   - e-mail me your suggestions at: andrew.berger.m@gmail.com
+
+   - you can support the project by small amount by sending bitcoins to my wallet
+
+1. If you are filling you did something wrong to me, do also something good, and it will be compensated somehow. :-)
+1. To get inspired in coding, launch the music:
+https://www.youtube.com/watch?v=lSTHPLQouOU
+
+
+Author thanks you for following the policy. :+1:
